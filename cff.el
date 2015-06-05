6db0b698dceefab3fac6110236eff4c8af39c61b
Alexey Veretennikov <alexey.veretennikov@afconsult.com>
2015-06-05 17:45:18 +0200

    added extraction of the git files

--- cff.el
+++ cff.el
@@ -196,12 +196,18 @@ to construct possible path to another file. Returns this directory short name
   (let* ((basename (file-name-base fname))
          ;; list of files to look for
          (filelist (mapcar #'(lambda (x) (funcall (cdr x) basename)) regexps))
+         (fregexp (concat "\\(" (mapconcat 'identity filelist "\\|") "\\)$"))
          ;; list of files in repo
          (git-list
           (mapcar #'(lambda (x) (concat top-dir x))
                       (split-string
-                       (shell-command-to-string (concat "git ls-files --full-name " top-dir))))))
-         nil))
+                       (shell-command-to-string (concat "git ls-files --full-name " top-dir)))))
+         (found nil)))
+  (dolist (f git-list)
+    (when (string-match fregexp f)
+      (push f found)))
+  (dolist (f found)
+    (message f)))
 
 (defun cff-find-other-file ()
   "Find the appropriate header, source or interface file for the current file"
