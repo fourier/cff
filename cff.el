32eac6d3607961b7eaa2090cf75e7dcedc2cf0c0
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2016-01-12 19:25:54 +0100

    1) Added custom mode for multiple selection when helm is not available; 2) Added cache for files selected in multiple selection; 3) Added possibility if nothing found find a file with a basename as a part of a file name

--- cff.el
+++ cff.el
@@ -1,10 +1,10 @@
-;;; cff.el --- Search of the C/C++ file header by the source and vice versa
+;;; cff.el --- Search of the C/C++ file header by the source and vice versa -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2015 Alexey Veretennikov
 ;;
 ;; Author: Alexey Veretennikov <alexey dot veretennikov at gmail dot com>
 ;; Created: 2015-06-02
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; Keywords: find-file
 ;; Package-Requires: ((cl-lib "0.5") (emacs "24"))
 ;; URL: https://github.com/fourier/eff
@@ -27,9 +27,9 @@
 ;;
 ;;; Commentary:
 ;;
-;; This is a replacement for the ff-find-other-file.  If the helm is loaded,
-;; uses it to provide possible multiple choices; otherwise just picks the
-;; first one.
+;; This is a simplified replacement for the ff-find-other-file.
+;; If the helm is loaded, uses it to provide possible multiple choices;
+;; otherwise provides with the special choice buffer.
 ;;
 ;; Usage:
 ;; Add the following to your .emacs file:
@@ -56,17 +56,19 @@
 ;;
 ;;; Code:
 
-(require 'cl-lib)
+(eval-when-compile (require 'cl-lib))
 ;; optional helm dependency
 (require 'helm nil t)
 
-(defvar cff-no-choice nil
+(defvar cff-use-helm-choice t
   "Determines what to do if there are several possible choices.
-If t, always to use the first one.")
+If t, use helm if available.
+If nil, always use internal choice mode.")
 
 (defvar cff-header-regexps '(("\\.h$" . (lambda (base) (concat base ".h")))
                              ("\\.hpp$" . (lambda (base) (concat base ".hpp")))
-                             ("\\.hxx$" . (lambda (base) (concat base ".hxx"))))
+                             ("\\.hxx$" . (lambda (base) (concat base ".hxx")))
+                             ("\\.hh$" . (lambda (base) (concat base ".hh"))))
   "Regexps used to determine if the file is a C/C++ header file.  List of pairs:
 regexp of the file extension and a function to construct filename by the given
 base name.")
@@ -95,6 +97,57 @@ List of pairs: regexp of the file extension anda function to construct filename
   "A list of short directory names to look sources in." )
 (defvar cff-interface-dirs '("if")
   "A list of short directory names to look interfaces in.")
+
+(defvar cff-mutiple-values-cache nil
+  "A cache for choices made if multiple alternatves were presented.")
+
+(define-derived-mode cff-choice-mode tabulated-list-mode "Cff choice"
+  "Major mode for selecting alternative files in cff"
+  (progn
+    (setq tabulated-list-format `[("File name" 0 nil)])
+    (tabulated-list-init-header)))
+
+(defun cff-choice-show (args process-choice)
+  "Create a buffer with a list of alternatives to choose from.
+ARGS is a list of strings, PROCESS-CHOICE is a function
+of one argument to process selected result"
+  (let ((buf (get-buffer-create "*Cff choices*")))
+    ;; first create and switch to the new empty buffer
+    (switch-to-buffer buf)
+    ;; turn on the choices mode (based on tabulated-list)
+    (cff-choice-mode)
+    ;; convert list of string arguments to the stuff accepted
+    ;; by the tabulated list
+    ;; string -> entry-list
+    ;; where entry-list is a (id vector-of-columns)
+    ;; since we have only one column(file name), the contents
+    ;; of this vector are according to requirements:
+    ;; (id list-of-properties)
+    ;; we keep fname as an id, and create only one property:
+    ;; the action to be performed when we press the row entry
+    ;; the action is a function accepting on argument: button
+    ;; we store the value of the id (it is a filename),
+    ;; destroy the choices window,
+    ;; and finally call the PROCESS-CHOICE functional argument
+    ;; with the filename
+    (setq tabulated-list-entries
+          (mapcar (lambda (fname)
+                    (list fname
+                          (vector (cons
+                                   fname
+                                   (list
+                                    'action
+                                    (lambda (button)
+                                      (let ((arg (tabulated-list-get-id)))
+                                        (quit-window t)
+                                        (funcall
+                                         process-choice
+                                         arg))))))))
+                  args))
+    (tabulated-list-print)
+    nil))
+  
+
 
 
 (defun cff-root-path (fname)
@@ -222,30 +275,46 @@ Argument REGEXPS list of regexps to find."
       results)))
 
 
-(defun cff-process-found (found)
-  "Post-processing of the FOUND list of files."
+(defun cff-process-one-found (original new)
+  "Given the ORIGINAL file open the corresponding NEW file.
+The ORIGINAL to NEW mapping will be stored in cache."
+  (find-file new)
+  (unless cff-mutiple-values-cache
+    (setq cff-mutiple-values-cache (make-hash-table :test 'equal)))
+  (puthash original new cff-mutiple-values-cache)
+  (puthash new original cff-mutiple-values-cache))
+
+(defun cff-process-all-found (fname found)
+  "Post-processing of the alternative for FNAME.
+Argument FOUND is the list of files alternatives
+headers or sources for FNAME."
   (cond ((not found) (message "Not found"))
         ;; found one alternative
         ((and found (= (length found) 1))
-         (find-file (car found)))
+         (cff-process-one-found fname (car found)))
         ;; found several alternatives
         (t
-         ;; if helm is available and cff-no-choice is true
-         (if (and (not cff-no-choice) (fboundp 'helm))
+         ;; if helm is available and user setting to use helm
+         ;; cff-use-helm-choice is true
+         (if (and cff-use-helm-choice (fboundp 'helm))
              ;; use helm if available
              (let ((some-helm-source
-                    '((name . "Possible alternatives")
-                      (candidates . found)
+                    `((name . "Possible alternatives")
+                      (candidates . ,found)
                       (action . (lambda (candidate)
-                                  (find-file candidate))))))
-               (helm :sources '(some-helm-source)))
-           ;; otherwise just pick the first file in the list
-           (find-file (car found))))))
+                                  (cff-process-one-found ,fname candidate))))))
+               (helm :sources some-helm-source))
+           ;; otherwise use internal choice mode
+           (cff-choice-show found (lambda (candidate)
+                                    (cff-process-one-found fname candidate)
+                                    ))))))
 
 
-(defun cff-find-in-git (fname top-dir regexps)
+(defun cff-find-in-git (fname top-dir regexps &optional last-resort)
   "Find the list of files in git repository based on FNAME in TOP-DIR.
-Using REGEXPS to construct a list of files based on FNAME."
+Using REGEXPS to construct a list of files based on FNAME.
+When LAST-RESORT is not nil, find all files which starts/ends with
+the same basename."
   (let* ((basename (file-name-base fname))
          ;; list of files to look for
          (filelist (mapcar #'(lambda (x) (funcall (cdr x) basename)) regexps))
@@ -263,12 +332,27 @@ Using REGEXPS to construct a list of files based on FNAME."
     (dolist (f git-list)
       (when (string-match fregexp f)
         (push f found)))
+    ;; when no files with exactly ours basename found, find all
+    ;; which starts with our basename
+    (when (and last-resort (not found))
+      (setq filelist (mapcar #'(lambda (x)
+                                 (funcall (cdr x)
+                                          (concat ".*" basename ".*"))) regexps))
+      (setq fregexp (concat "/\\(" (mapconcat 'identity filelist "\\|") "\\)$"))
+    (dolist (f git-list)
+      (when (string-match fregexp f)
+        (push f found)))
+    ;; warn the user about not exact match
+    (when found
+      (message "Not exact match")))
     found))
 
 ;;;###autoload
-(defun cff-find-other-file ()
-  "Find the appropriate header, source or interface file for the current file."
-  (interactive)
+(defun cff-find-other-file (prefix)
+  "Find the appropriate header, source or interface file for the current file.
+If called with a PREFIX argument, force choice window
+for multiple possible file variants; otherwise use the cached file name."
+  (interactive "P")
   (let* ((fname (expand-file-name (buffer-file-name)))     ; full file name
          (ftype (cff-file-type fname))  ; file type
          (fdir (file-name-directory fname)) ; directory where the file is
@@ -279,57 +363,65 @@ Using REGEXPS to construct a list of files based on FNAME."
          (repo-type (cdr top-dir-pair))       ; repo type
          (replacement (cff-find-replacement fname ftype))
          (regexps (make-hash-table)))
-    (puthash 'header cff-source-regexps regexps)
-    (puthash 'source cff-header-regexps regexps)
-    (puthash 'interface cff-source-regexps regexps)
-    (if (eql ftype 'unknown)
-        (message "Unknown file type")
-      ;; first find in closest directrories up in file hierarchy
-      (let ((found
-             (cond ((eql ftype 'header)
-                    (cff-find-files-with-predicate top-dir fdir cff-source-dirs
-                                                   #'(lambda (x)
-                                                       (cff-is-source-for-header
-                                                        x fname))))
-                   ((eql ftype 'source)
-                    (cff-find-files-with-predicate top-dir fdir cff-header-dirs
-                                                   #'(lambda (x)
-                                                       (cff-is-header-for-source
-                                                        x fname))))
-                   ((eql ftype 'interface)
-                    (cff-find-files-with-predicate
-                     top-dir fdir cff-source-dirs
-                     #'(lambda (x)
-                         (cff-is-source-for-interface x fname))))
-                   (t nil)))
-            ;; next try to find by replacing strings in path (like src->inc)
-            (found-in-path
-             (cond ((eql ftype 'header)
-                    (cff-find-files-with-path fname replacement cff-source-dirs
-                                              cff-source-regexps))
-                   ((eql ftype 'source)
-                    (cff-find-files-with-path fname replacement cff-header-dirs
-                                              cff-header-regexps))
-                   ((eql ftype 'interface)
-                    (cff-find-files-with-path fname replacement cff-source-dirs
-                                              cff-source-regexps))))
-            ;; and at last find all similar in git
-            (found-in-git (when (eql repo-type 'git)
-                            (cff-find-in-git fname top-dir (gethash ftype
-                                                                    regexps)))))
-        (when found-in-path
-          (dolist (f found-in-path)
-            ;; they may be already in results, so push only new
-            (cl-pushnew f found :test 'string=)))
-        ;; add all found in git repo
-        (when found-in-git
-          (dolist (f found-in-git)
-            ;; they may be already in results, so push only new
-            (cl-pushnew f found :test 'string=)))
-        (when found
-          ;; process results
-          (setf found (nreverse found)))
-        (cff-process-found found)))))
+    ;; test if we already have this file in stored in cache
+    (if (and cff-mutiple-values-cache
+             (not prefix)
+             (gethash fname cff-mutiple-values-cache))
+        (find-file (gethash fname cff-mutiple-values-cache))
+      ;; otherwise do normal processing
+      (puthash 'header cff-source-regexps regexps)
+      (puthash 'source cff-header-regexps regexps)
+      (puthash 'interface cff-source-regexps regexps)
+      (if (eql ftype 'unknown)
+          (message "Unknown file type")
+        ;; first find in closest directrories up in file hierarchy
+        (let* ((found
+                (cond ((eql ftype 'header)
+                       (cff-find-files-with-predicate top-dir fdir cff-source-dirs
+                                                      #'(lambda (x)
+                                                          (cff-is-source-for-header
+                                                           x fname))))
+                      ((eql ftype 'source)
+                       (cff-find-files-with-predicate top-dir fdir cff-header-dirs
+                                                      #'(lambda (x)
+                                                          (cff-is-header-for-source
+                                                           x fname))))
+                      ((eql ftype 'interface)
+                       (cff-find-files-with-predicate
+                        top-dir fdir cff-source-dirs
+                        #'(lambda (x)
+                            (cff-is-source-for-interface x fname))))
+                      (t nil)))
+               ;; next try to find by replacing strings in path (like src->inc)
+               (found-in-path
+                (cond ((eql ftype 'header)
+                       (cff-find-files-with-path fname replacement cff-source-dirs
+                                                 cff-source-regexps))
+                      ((eql ftype 'source)
+                       (cff-find-files-with-path fname replacement cff-header-dirs
+                                                 cff-header-regexps))
+                      ((eql ftype 'interface)
+                       (cff-find-files-with-path fname replacement cff-source-dirs
+                                                 cff-source-regexps))))
+               ;; and at last find all similar in git
+               (found-in-git (when (eql repo-type 'git)
+                               (cff-find-in-git fname
+                                                top-dir
+                                                (gethash ftype regexps)
+                                                (not (or found found-in-path))))))
+          (when found-in-path
+            (dolist (f found-in-path)
+              ;; they may be already in results, so push only new
+              (cl-pushnew f found :test 'string=)))
+          ;; add all found in git repo
+          (when found-in-git
+            (dolist (f found-in-git)
+              ;; they may be already in results, so push only new
+              (cl-pushnew f found :test 'string=)))
+          (when found
+            ;; process results
+            (setf found (nreverse found)))
+          (cff-process-all-found fname found))))))
 
 
 ;; algorithm:
